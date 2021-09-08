ac3d0a200e78a2b415937826c6ee023aa8148afc
Paulopdm <paulo.mfn@gmail.com>
2021-09-08 15:41:21 -0300

    aplicando camada security

---
+++ lojadosgames/src/main/java/com/generation/lojadosgames/repository/UsuarioRepository.java
@@ -0,0 +1,13 @@
+package com.generation.lojadosgames.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.generation.lojadosgames.model.Usuario;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+	public Optional<Usuario> findByUsuario(String usuario);
+}
